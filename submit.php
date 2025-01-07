c2ea40cf190738283d89ac48a0f48aba95a051e5
MdanzDev <nightmaremoon297@gmail.com>
2025-01-07 09:51:45 +0800

    Update submit.php

--- submit.php
+++ submit.php
@@ -4,9 +4,11 @@ require 'env-loader.php';
 $pdo = new PDO('sqlite:applications.db');
 $stmt = $pdo->prepare("INSERT INTO applications (name, age, state, phone, email) VALUES (?, ?, ?, ?, ?)");
 $stmt->execute([$_POST['name'], $_POST['age'], $_POST['state'], $_POST['phone'], $_POST['email']]);
-   echo "<p>Success! We will email you soon.</p>";
 
-    // Redirect to index.html after 3 seconds
-    header("refresh:3; url=index.html");
-    exit();
+// Redirect to index.html after 3 seconds
+header("refresh:3; url=index.html");
+exit();
+
+// Success message will now be printed after the redirect
+echo "<p>Success! We will email you soon.</p>";
 ?>

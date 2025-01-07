47cadd902a6751c0530973b5c6a43d004861eafd
MdanzDev <nightmaremoon297@gmail.com>
2025-01-07 09:47:04 +0800

    Update submit.php

--- submit.php
+++ submit.php
@@ -4,5 +4,9 @@ require 'env-loader.php';
 $pdo = new PDO('sqlite:applications.db');
 $stmt = $pdo->prepare("INSERT INTO applications (name, age, state, phone, email) VALUES (?, ?, ?, ?, ?)");
 $stmt->execute([$_POST['name'], $_POST['age'], $_POST['state'], $_POST['phone'], $_POST['email']]);
-echo "Application submitted successfully!";
+   echo "<p>Success! We will email you soon.</p>";
+
+    // Redirect to index.html after 3 seconds
+    header("refresh:3; url=index.html");
+    exit();
 ?>
